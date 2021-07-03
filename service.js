e057ea22e35b007e396c17481da42783c61aaa48
applesky2 <3021984098@qq.com>
2021-07-03 16:56:50 +0800

    node请求静态资源html&node作为客户端请求第三方服务器

--- service.js
+++ service.js
@@ -1,10 +1,21 @@
 const http = require("http");
 const url = require('url');
-const util = require('util');
+let fs = require('fs');
 const service =  http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end(util.inspect(url.parse(req.url)));
+    const pathName = url.parse(req.url).pathname;
+    fs.readFile(pathName.substring(1), (err, data) => {
+        if (err) {
+            res.writeHead(404, {
+                "Content-Type": "text/html"
+            });
+        } else {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.write(data.toString());
+        }
+        res.end();
+    })
 });
 service.listen(3000, '127.0.0.1', ()=> {
     console.log('服务器已启动,通过http:127.0.0.1:3000/来打开页面');
